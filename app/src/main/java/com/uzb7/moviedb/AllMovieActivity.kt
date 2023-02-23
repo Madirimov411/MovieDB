547c4960612e0beb8d4e4e4e40154a07a49f2e16
BackendDev07 <browhith3.uz@gmail.com>
2023-02-23 18:56:08 +0500

    uzb7:Mansurbek

---
+++ app/src/main/java/com/uzb7/moviedb/AllMovieActivity.kt
@@ -0,0 +1,11 @@
+package com.uzb7.moviedb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AllMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_movie)
+    }
+}
